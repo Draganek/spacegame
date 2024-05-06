502a964d673ea1b7891f8b756245d05e01ba0562
Draganek <Radek.Draganek@gmail.com>
2024-05-06 21:20:18 +0200

    enemy creation/movement/kill design and bulet shot/move/colision

--- src/MobileSpaceGame/MobileSpaceGame.tsx
+++ src/MobileSpaceGame/MobileSpaceGame.tsx
@@ -6,9 +6,10 @@ import scoreImage from "../img/coin.png"
 export const MobileSpaceGame = () => {
     const [bullets, setBullets] = useState<HTMLDivElement[]>([]);
     const [enemies, setEnemies] = useState<HTMLDivElement[]>([]);
-    const [level, setLevel] = useState<number>(1);
+    const [level, setLevel] = useState<number>(3);
     const [lifes, setLifes] = useState<number>(3);
     const [score, setScore] = useState<number>(0);
+    const [bulletSpeed, SetBulletSpeed] = useState<number>(1);
     const [gameStarted, setGameStarted] = useState<boolean>(false);
     const [newLevel, setNewLevel] = useState<boolean>(false);
     const playerRef = useRef<HTMLDivElement>(null);
@@ -17,6 +18,10 @@ export const MobileSpaceGame = () => {
     const [shipPosition, setShipPosition] = useState({ x: "50%", y: '0%' });
 
     useEffect(() => {
+        const enemyInterval = setInterval(createEnemy, 1000 - 15 * level);
+        const moveEnemiesInterval = setInterval(moveEnemies, 50 - 5 * level);
+        const bulletShotInterval = setInterval(createBullet, 1000 * bulletSpeed);
+        const bulletInterval = setInterval(moveBullets, 50);
         const handleTouchMove = (e: TouchEvent) => {
             e.preventDefault();
             const touch = e.touches[0];
@@ -25,15 +30,110 @@ export const MobileSpaceGame = () => {
             else {
                 setShipPosition({ x: `${touch.clientX}px`, y: `${-touch.clientY + boardRef.current!.offsetHeight}px` });
             }
-
         };
-
         window.addEventListener('touchmove', handleTouchMove, { passive: false });
 
         return () => {
             window.removeEventListener('touchmove', handleTouchMove);
+            clearInterval(bulletInterval);
+            clearInterval(enemyInterval);
+            clearInterval(moveEnemiesInterval);
+            clearInterval(bulletShotInterval);
         };
-    }, []);
+    }, [lifes, gameStarted, newLevel, level, bulletSpeed]);
+
+
+    const createEnemy = () => {
+        const shouldCreate = Math.round(Math.random());
+        if (shouldCreate) {
+            const enemy = document.createElement('div');
+            enemy.className = "enemy";
+            enemy.style.top = '-40px';
+            enemy.style.left = `${Math.floor(Math.random() * (boardRef.current!.offsetWidth - 120) + 60)}px`
+
+            boardRef.current!.append(enemy);
+            setEnemies(prevEnemies => [...prevEnemies, enemy]);
+        }
+    }
+
+    const moveEnemies = () => {
+        setEnemies(prevEnemies => {
+            const updatedEnemies: HTMLDivElement[] = [];
+            prevEnemies.forEach(enemy => {
+                const newTop = enemy.offsetTop + 2;
+                enemy.style.top = `${newTop}px`;
+                if (newTop < boardRef.current!.offsetHeight) {
+                    updatedEnemies.push(enemy);
+                } else {
+                    enemy.remove();
+                    setLifes(prevLifes => prevLifes - 1)
+                }
+            });
+            return updatedEnemies;
+        });
+    };
+
+    const createBullet = () => {
+        const bullet = document.createElement('div');
+        bullet.className = 'bullet';
+        bullet.style.left = `${playerRef.current!.offsetLeft}px`;
+        bullet.style.top = `${playerRef.current!.offsetTop}px`;
+        boardRef.current!.appendChild(bullet);
+        setBullets(prevBullets => [...prevBullets, bullet]);
+    }
+
+    const moveBullets = () => {
+        setBullets(prevBullets => {
+            const updatedBullets: HTMLDivElement[] = [];
+            prevBullets.forEach(bullet => {
+                if (bullet.offsetTop >= 0) {
+                    const newTop = bullet.offsetTop - 5;
+                    bullet.style.top = `${newTop}px`;
+                    updatedBullets.push(bullet);
+                    checkBulletCollision(bullet);
+                } else {
+                    bullet.remove();
+                }
+            });
+            return updatedBullets;
+        });
+    };
+
+    const checkBulletCollision = (bullet: HTMLDivElement) => {
+        setEnemies(prevEnemies => {
+            const updatedEnemies = prevEnemies.filter(enemy => {
+                const bulletPosition = bullet.getBoundingClientRect();
+                const enemyPosition = enemy.getBoundingClientRect();
+                if (
+                    bulletPosition.left < enemyPosition.right &&
+                    bulletPosition.right > enemyPosition.left &&
+                    bulletPosition.top < enemyPosition.bottom &&
+                    bulletPosition.bottom > enemyPosition.top
+                ) {
+                    makeExplosion(enemy.offsetLeft, enemy.offsetTop);
+                    setScore(prevScore => prevScore + 1);
+                    enemy.remove();
+                    bullet.remove();
+                    return false;
+                }
+                return true;
+            });
+            return updatedEnemies;
+        });
+    };
+
+    const makeExplosion = (left: number, top: number) => {
+        const explosion = document.createElement('div');
+        explosion.className = 'explosion';
+        explosion.style.left = `${left}px`
+        explosion.style.top = `${top}px`
+        boardRef.current!.appendChild(explosion)
+
+        setTimeout(() => {
+            explosion.remove();
+        }, 500)
+
+    }
 
     return (
         <div>
