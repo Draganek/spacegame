36ffe2c26369763decc537715f11c07748b7a87b
Draganek <Radek.Draganek@gmail.com>
2024-07-01 23:54:42 +0200

    New enemy start config

--- src/gameConfig/levelConfigurations.tsx
+++ src/gameConfig/levelConfigurations.tsx
@@ -11,3 +11,17 @@ export const levelConfigurations: any = {
     10: { count: 30, speed: 0, frequency: 10 },
     11: { count: 300, speed: 100, frequency: 100 },
 };
+
+export const slantersConfig: any = {
+    1: { count: 5, speed: 0, frequency: 0, slanter: 5},
+    2: { count: 10, speed: 1, frequency: 1},
+    3: { count: 15, speed: 2, frequency: 2 },
+    4: { count: 20, speed: 3, frequency: 2 },
+    5: { count: 20, speed: 3, frequency: 3 },
+    6: { count: 20, speed: 4, frequency: 3 },
+    7: { count: 25, speed: 4, frequency: 3 },
+    8: { count: 25, speed: 4, frequency: 4 },
+    9: { count: 25, speed: 5, frequency: 4 },
+    10: { count: 30, speed: 0, frequency: 10 },
+    11: { count: 300, speed: 100, frequency: 100 },
+};
